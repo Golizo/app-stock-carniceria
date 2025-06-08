fd53ebc1a0f79d683c3e0de7bf495e1154a27de6
Golizo <gonzalo.sanmartinfierro@gmail.com>
2025-06-08 16:50:18 -0400

    Delete src directory

--- src/AppStockCarniceria.jsx
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-import { Card, CardContent } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from "@/components/ui/table";
-
-// ... (el contenido completo del documento va aquí; por simplicidad, omito repetirlo aquí)
